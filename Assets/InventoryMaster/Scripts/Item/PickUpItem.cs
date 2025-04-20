1f8a8d43e04d5a6b2c1cb95e971f2dce62a8436f
Kenza NAIT ATMANE <naitatmane.kenza2003@gmail.com>
2025-04-20 12:21:09 +0200

    Ajout des items

--- Assets/InventoryMaster/Scripts/Item/PickUpItem.cs
+++ Assets/InventoryMaster/Scripts/Item/PickUpItem.cs
@@ -17,24 +17,26 @@ public class PickUpItem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_inventory != null && Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && _inventory != null)
         {
             float distance = Vector3.Distance(this.gameObject.transform.position, _player.transform.position);
-
-            if (distance <= 3)
-            {
-                bool check = _inventory.checkIfItemAllreadyExist(item.itemID, item.itemValue);
-                if (check)
-                    Destroy(this.gameObject);
-                else if (_inventory.ItemsInInventory.Count < (_inventory.width * _inventory.height))
+            
+                if (distance <= 15.0f)
                 {
-                    _inventory.addItemToInventory(item.itemID, item.itemValue);
-                    _inventory.updateItemList();
-                    _inventory.stackableSettings();
-                    Destroy(this.gameObject);
-                }
+                    bool check = _inventory.checkIfItemAllreadyExist(item.itemID, item.itemValue);
+                    if (check)
+                        Destroy(this.gameObject);
+                    else if (_inventory.ItemsInInventory.Count < (_inventory.width * _inventory.height))
+                    {
+                        _inventory.addItemToInventory(item.itemID, item.itemValue);
+                        _inventory.updateItemList();
+                        _inventory.stackableSettings();
+                        Destroy(this.gameObject);
+                    }
 
-            }
+                }
+            
+          
         }
     }
 
