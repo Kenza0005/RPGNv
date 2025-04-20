99f15dd5d5307eaab846e251be48f04724f37787
Kenza NAIT ATMANE <naitatmane.kenza2003@gmail.com>
2025-04-20 17:16:08 +0200

    debut attack elle ne marche pas encore!

--- Assets/CharacterMotion.cs
+++ Assets/CharacterMotion.cs
@@ -6,11 +6,16 @@ public class CharacterMotion : MonoBehaviour
 {
     // Animation du personnage 
     public Animation animations;
-  // Vitesse de déplacement du personnage
+    // Vitesse de déplacement du personnage
     public float walkSpeed = 2f;
     public float runSpeed = 5f;
     public float turnSpeed = 200f;
-
+    // Variable d'attaque
+    // le delais entre chaque attaque
+    public float attackCooldown;
+    private bool isAttacking = false;
+    // le temps restant pour attaque a nouveau 
+    private float currentCooldown;
 
     // Inputs du joueur
     public KeyCode inputFront = KeyCode.W;
@@ -31,67 +36,110 @@ public class CharacterMotion : MonoBehaviour
             new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.min.y - 0.1f, playerCollider.bounds.center.z),
             0.08f,
             3
-             // Layer 3 uniquement
+        // Layer 3 uniquement
         );
     }
     void Start()
     {
         animations = gameObject.GetComponent<Animation>();
         playerCollider = gameObject.GetComponent<CapsuleCollider>();
-       
+
     }
 
     void Update()
     {
-       if(!isDead)
-       {
-             if (Input.GetKey(inputFront) && Input.GetKey(inputRun))
+        if (!isDead)
         {
-            transform.Translate(0,0,runSpeed *Time.deltaTime);
-            animations.Play("run");
-        }
-        //Marcher-----------------------------------------------------------------------
-        if (Input.GetKey(inputFront) && !Input.GetKey(inputRun))
-        {
-            transform.Translate(0,0,walkSpeed *Time.deltaTime);
-            animations.Play("walk");
-        }
-        //-----------------------------------------------------------------------
-        if (Input.GetKey(inputBack))
-        {
-            transform.Translate(0,0,-(walkSpeed/2) *Time.deltaTime);
-            animations.Play("walk");
-        }
-        //Rotation_Gauche-----------------------------------------------------------------------
-        if (Input.GetKey(inputLeft))
-        {
-            transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
-        }
-        //Rotation_Droite-----------------------------------------------------------------------
-        if (Input.GetKey(inputRight))
-        {
-            transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
-        }
-        // si on ne fais rien :----------------------------------------------------------------------------
-        if (!Input.GetKey(inputFront) && !Input.GetKey(inputBack))
-        {
-           animations.Play("idle");
-        }
-                //-----------------------------------------------------------------------
-        // Saut
-        if (Input.GetKeyDown(inputJump) && IsGrounded())
-        {
-            // preparation du saut 
-            Vector3 v =  gameObject.GetComponent<Rigidbody>().velocity;
-            v.y = jumpSpeed.y;  
-            // le saut:
-            gameObject.GetComponent<Rigidbody>().velocity = jumpSpeed;
-          
-        }
-       }
 
-       
+            if (Input.GetKey(inputFront) && Input.GetKey(inputRun))
+            {
+                transform.Translate(0, 0, runSpeed * Time.deltaTime);
+                if (!isAttacking)
+                {
+                    animations.Play("run");
+                }
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    Attack();
+                }
+
+            }
+            //Marcher-----------------------------------------------------------------------
+            if (Input.GetKey(inputFront) && !Input.GetKey(inputRun))
+            {
+                transform.Translate(0, 0, walkSpeed * Time.deltaTime);
+                if (!isAttacking)
+                {
+                    animations.Play("walk");
+                }
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    Attack();
+                }
+            }
+            //-----------------------------------------------------------------------
+            if (Input.GetKey(inputBack))
+            {
+                transform.Translate(0, 0, -(walkSpeed / 2) * Time.deltaTime);
+                if (!isAttacking)
+                {
+                    animations.Play("walk");
+                }
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    Attack();
+                }
+
+            }
+            //Rotation_Gauche-----------------------------------------------------------------------
+            if (Input.GetKey(inputLeft))
+            {
+                transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+            }
+            //Rotation_Droite-----------------------------------------------------------------------
+            if (Input.GetKey(inputRight))
+            {
+                transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+            }
+            // si on ne fais rien :----------------------------------------------------------------------------
+            if (!Input.GetKey(inputFront) && !Input.GetKey(inputBack))
+            {
+                if (!isAttacking)
+                {
+                    animations.Play("idle");
+                }
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    Attack();
+                }
+
+            }
+            //-----------------------------------------------------------------------
+            // Saut
+            if (Input.GetKeyDown(inputJump) && IsGrounded())
+            {
+                // preparation du saut 
+                Vector3 v = gameObject.GetComponent<Rigidbody>().velocity;
+                v.y = jumpSpeed.y;
+                // le saut:
+                gameObject.GetComponent<Rigidbody>().velocity = jumpSpeed;
+
+            }
+        }
+        if (isAttacking) { currentCooldown -= Time.deltaTime; }
+        if (currentCooldown <= 0)
+        {
+            currentCooldown = attackCooldown;
+            isAttacking = false;
+
+        }
+
     }
-    
+    public void Attack()
+    {
+        isAttacking = true;
+        animations.Play("attack");
+        Debug.Log("Attaque lancée !");
+    }
 
 }
